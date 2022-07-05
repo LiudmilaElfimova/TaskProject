975264cd26f9ba9efd5fa02526ffdaa32cb92d42
LiudmilaElfimova <liudmielfimova@yandex.com>
2022-07-05 11:41:42 +0200

    initial commit

---
+++ src/main/java/com/liudmila/spring/spring_boot_task/SpringBootTaskApplication.java
@@ -0,0 +1,13 @@
+package com.liudmila.spring.spring_boot_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liudmila/spring/spring_boot_task/SpringBootTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liudmila.spring.spring_boot_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
